30e38d7f88151de17ed0744be28c1454de7b6d5e
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-02-13 16:58:53 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,7 +1,18 @@
 class Patient
+  attr_reader :name
+
   @@all = []
 
   def initialize(name)
     @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+
   end
 end
