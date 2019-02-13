654b30039e058766b3806f3519c4fe2cea4b05f8
Alex Juszczakiewicz <curiositylex@protonmail.com>
2019-02-13 16:34:37 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist
+  @@all = Array.new
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_song(name, genre)
+    Song.new(name, genre, self)
+  end
+
+  def songs
+      Song.all.select{|song| song.artist == self}
+  end
+
+  def self.all
+    @@all
+  end
+end
